be952c42de8f7ad353f3fc8d0c6221439a20b391
capybra12-maker <capybra12@gmail.com>
2026-09-06 11:40:52 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: isProd ? "/nezeal-shintal-wedding1" : "",
+  assetPrefix: isProd ? "/nezeal-shintal-wedding1/" : "",
+};
+
+export default nextConfig;
